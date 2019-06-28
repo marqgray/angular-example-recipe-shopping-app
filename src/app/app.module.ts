8ebe9f9ae5c7b7c85dd4d7301a3592637a07e4c3
Marquis Sandifer <marquis.s.sandifer@gmail.com>
2019-06-28 15:07:21 -0500

    Added generated basic components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingListEditorComponent} from './shopping-list/shopping-list-editor/shopping-list-editor.component';
+import {RecipeListComponent} from './recipe-book/recipe-list/recipe-list.component';
+import {RecipeBookComponent} from './recipe-book/recipe-book.component';
+import {RecipeItemComponent} from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import {RecipeDetailComponent} from './recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditorComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
